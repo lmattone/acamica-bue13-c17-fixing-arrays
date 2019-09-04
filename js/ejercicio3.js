3e51f389f007dacb8e0ef22e6290dd5bd9878a41
lmattone <mattone.leila@gmail.com>
2019-09-04 20:13:04 -0300

    Clase de Arrays

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,17 +1,28 @@
 console.log('Ejercicio 3: ');
+/*Ejercicio 3: 
+Mi lista de reproduccion por ahora tiene 0 canciones porque esta vacia.
+Ahora ya tengo mi album de Soda listo para reproducirse.En la ciudad de la furia,
+En rito,Hombre al agua,En el septimo dia con un total de 4 canciones.
+La proxima cancion a reproducir es En el septimo dia
+Y restan en mi lista: En la ciudad de la furia,En rito,Hombre al agua*/
 
 var listaDeReproduccion = [];
 
-console.log('Mi lista de reproduccion por ahora tiene ' + listaDeReproduccion.length() + ' canciones porque esta vacia.');
+console.log('Mi lista de reproduccion por ahora tiene.' + listaDeReproduccion.length + 'canciones porque esta vacia.');
 
 listaDeReproduccion.push('En la ciudad de la furia');
-listaDeReproduccion.pull('En rito');
+listaDeReproduccion.push('En rito');
 listaDeReproduccion.push('Hombre al agua');
-listaDeReproduccion.push('En el septimo dia);
+listaDeReproduccion.push('En el septimo dia');
 
 console.log('Ahora ya tengo mi album de Soda listo para reproducirse.' + listaDeReproduccion + ' con un total de ' + listaDeReproduccion.length + ' canciones.');
 
-listaDeReproduccion.plop();
+listaDeReproduccion.pop();
+listaDeReproduccion.push('En el septimo dia');
+
+
+
+var proximaCancion = listaDeReproduccion[3];
 
 console.log('La proxima cancion a reproducir es ' + proximaCancion);
 console.log('Y restan en mi lista: ' + listaDeReproduccion);
--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,19 +1,26 @@
+/*Ejercicio 4: 
+No recuerdo en que posicion de mi biblioteca deje el libro de Sherlock. Harry Potter,Game of Thrones,Sherlock,Dracula
+Estaba en la posicion: 2
+Porque en verdad solo era la tapa, adentro esta el Libro de la Selva.
+Me regalaron para navidad el libro de La divina comedia. La voy a agregar al fondo de mi biblioteca
+Ahora si, finalmente mi biblioteca quedo completa: Harry Potter,Game of Thrones,Libro de la Selva,Dracula,La divina comedia*/
+
 console.log('Ejercicio 4: ');
 
 var misLibros = ['Harry Potter', 'Game of Thrones', 'Sherlock', 'Dracula'];
 
 console.log('No recuerdo en que posicion de mi biblioteca deje el libro de Sherlock. ' + misLibros);
 
-var posicion = 0;
+var posicion = misLibros.indexOf('Sherlock');
 
 console.log('Estaba en la posicion: ' + posicion);
 
 console.log('Porque en verdad solo era la tapa, adentro esta el Libro de la Selva.');
 
-misLibros[] = 'Libro de la Selva';
+misLibros.push('Libro de la Selva');
 
 console.log('Me regalaron para navidad el libro de La divina comedia. La voy a agregar al fondo de mi biblioteca');
 
-
+misLibros.push('La divina comedia');
 
 console.log('Ahora si, finalmente mi biblioteca quedo completa: ' + misLibros);
